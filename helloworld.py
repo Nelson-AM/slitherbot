5e89e6015dccb827e3ef206bbc1a2d215449bacc
Marysia <marysia@live.nl>
2016-08-13 15:42:33 +0200

    some changes

--- helloworld.py
+++ helloworld.py
@@ -2,6 +2,34 @@ import pyautogui as pag
 import cv2
 import time
 import webbrowser as wb
+from random import randint
+
+
+def random_direction(): 
+    a = randint(0,4)
+    if a == 0: 
+        print 'Left!'
+        timeoutleft = time.time() + 5
+        while time.time() < timeoutleft:
+            pag.keyDown('left')
+    if a == 1: 
+        print 'Right!'
+        timeoutleft = time.time() + 5
+        while time.time() < timeoutright:
+            pag.keyDown('right')
+    if a == 2: 
+        print 'Up!'
+        timeoutleft = time.time() + 5
+        while time.time() < timeoutright:
+            pag.keyDown('up')
+    if a == 3: 
+        print 'Down!'
+        timeoutleft = time.time() + 5
+        while time.time() < timeoutright:
+            pag.keyDown('down')
+    if a == 4: 
+        time.sleep(5)
+        
 
 wb.open("http://slither.io/")
 time.sleep(15)
@@ -26,4 +54,9 @@ while time.time() < timeoutright:
     print time.time()
 
 pag.keyUp('right')
+
+timeleft = time.time() + 40
+while time.time() < timeleft: 
+    random_direction()
+    
 print("And we\'re done!")
